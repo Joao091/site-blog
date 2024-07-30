ddc96e612c0db19c81d73c1184efe6c4fbb80347
joao091 <joao.pedrosilvade.sa@escola.pr.gov.br>
2024-07-30 11:27:43 -0300

    commit

---
+++ src/main/java/com/eleodoro/demo/SiteBlogApplication.java
@@ -0,0 +1,13 @@
+package com.eleodoro.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteBlogApplication.class, args);
+	}
+
+}
